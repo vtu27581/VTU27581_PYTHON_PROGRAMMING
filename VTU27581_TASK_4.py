4d7cd3aec9108aca8e0425aa525b48163a06260b
vtu27581 <vtu27581@veltech.edu.in>
2025-11-05 23:41:49 +0530

    VTU27581_TASK_4.py

---
+++ VTU27581_TASK_4.py
@@ -0,0 +1,23 @@
+#student marks manager
+
+marks = [78, 36,89,92,69]
+
+Print("Marks:", marks)
+
+#Highest and lowest
+
+print("Highest mark, max(marks))
+
+print("lowest mark:", min(marks))
+
+#Average
+
+average = sum (marks)/len (manks)
+
+print("Average mark:", average)
+
+#sort in ascending order
+
+marks.sort()
+
+Print("sorted marks:, marks)
