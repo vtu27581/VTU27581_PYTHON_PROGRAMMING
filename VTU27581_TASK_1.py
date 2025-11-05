15b29c0895227b24d34ed6242638a8edba033896
vtu27581 <vtu27581@veltech.edu.in>
2025-11-05 23:38:11 +0530

    VTU27581_TASK_1.py

---
+++ VTU27581_TASK_1.py
@@ -0,0 +1,19 @@
+Price 1 = float (input("enter first price:"))
+
+price2= float (input ("enter second number: "))
+
+total = pricel + price 2
+
+difference = price-price z
+
+product = price, price 2
+
+division = pricell price 2
+
+print("total:", total),
+
+print ("difference: difference)
+
+print ("product", product)
+
+print("division", division)
