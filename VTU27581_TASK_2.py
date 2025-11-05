5f9fa15befa7f36097b1d2000a1272ef570c27ab
vtu27581 <vtu27581@veltech.edu.in>
2025-11-05 23:40:07 +0530

    VTU27581_TASK_2.py

---
+++ VTU27581_TASK_2.py
@@ -0,0 +1,13 @@
+level = float("Enter the coater level (in meters): "))
+
+if level <50:
+
+print("low water level")
+
+Elif 50<=level<=80:
+
+Print ("Normal water level")
+
+else:
+
+Print (High water level- caution")
