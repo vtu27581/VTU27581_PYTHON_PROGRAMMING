7829e8ab2cc63d8b872663f1969d418c3e2fc1ee
vtu27581 <vtu27581@veltech.edu.in>
2025-11-05 23:48:10 +0530

    VTU27581_TASK_9.py

---
+++ VTU27581_TASK_9.py
@@ -0,0 +1,17 @@
+try:
+
+num1= float(input("Enter numerator:"))
+
+num2=float(input("Enter demominator:"))
+
+result = num1/num2
+
+print ("Result=", result)
+
+except zerodivision Error:
+
+print("error! Division by zero is not allowed.")
+
+finally:
+
+print("Division operations completed:")
